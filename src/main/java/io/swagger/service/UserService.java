45041f7113ef81d589942d041866f4a2f3eb9491
kienguyen01 <kienguyen01@gmail.com>
2021-06-03 03:58:44 +0700

    JWT

--- src/main/java/io/swagger/service/UserService.java
+++ src/main/java/io/swagger/service/UserService.java
@@ -1,4 +1,23 @@
 package io.swagger.service;
 
+import io.swagger.model.dbUser;
+import io.swagger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public dbUser addUser(dbUser user){
+        userRepository.save(user);
+        return user;
+    }
+
+    public List<dbUser> getUsers(){
+        return (List<dbUser>) userRepository.findAll();
+    }
 }
