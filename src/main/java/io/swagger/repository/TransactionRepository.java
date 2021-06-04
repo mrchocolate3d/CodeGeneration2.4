b60c8887054a105ff604e9561d07179fe9e785be
phatle281200 <tple281200@gmail.com>
2021-06-04 12:01:38 +0200

    repo+service
    no need loginService

--- src/main/java/io/swagger/repository/TransactionRepository.java
+++ src/main/java/io/swagger/repository/TransactionRepository.java
@@ -1,15 +1,11 @@
 package io.swagger.repository;
 
 import io.swagger.model.dbTransaction;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends CrudRepository<dbTransaction,Long> {
-//    @Query("//")
-    Iterable<dbTransaction>getTransactionsByIBAN(String IBAN);
-
-    Iterable<dbTransaction>getTransactionsByID(long id);
-
+    //dbTransaction findTransactionByID(Long id);
+    Iterable<dbTransaction> getTransactionsByIBAN(String iban);
 }
