a3d74a8314e4dc0479e9992dc5d5c77cae8458bf
Seyifunmi Gandonu <43957746+mrchocolate3d@users.noreply.github.com>
2021-06-06 22:25:43 +0200

    Working version

--- src/main/java/io/swagger/model/User.java
+++ src/main/java/io/swagger/model/User.java
@@ -49,17 +49,6 @@ public class User   {
 
 
 
-
-  public User(Long id, String username, String firstName, String lastName, String email, String phone, Integer transactionLimit) {
-    this.id = id;
-    this.username = username;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.email = email;
-    this.phone = phone;
-    this.transactionLimit = transactionLimit;
-  }
-
   public User id(Long id) {
     this.id = id;
     return this;
